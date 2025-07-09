e5f0e926209b63d7deb6ec56708786fee6d576e4
geonppang0323 <wnsgus2303@gmail.com>
2025-07-09 12:40:02 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.02 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
@@ -5,16 +5,19 @@ using namespace std;
 vector<int> solution(long long n) {
     vector<int> result;
 
+    // n의 각 자릿수를 뒤에서부터 추출해 벡터에 저장
     while (n > 0) {
-        result.push_back(n % 10);  
-        n /= 10;                   
+        result.push_back(n % 10);  // 마지막 자릿수
+        n /= 10;                   // 자릿수 하나 줄이기
     }
 
     return result;
 }
+
+// 테스트용 main 함수
 int main() {
     long long n;
-    cout << "자연수 n을 입력하세요 (최대 10조): ";
+    cout << "자연수 n 입력 (최대 10조): ";
     cin >> n;
 
     vector<int> answer = solution(n);
@@ -27,3 +30,4 @@ int main() {
 
     return 0;
 }
+
